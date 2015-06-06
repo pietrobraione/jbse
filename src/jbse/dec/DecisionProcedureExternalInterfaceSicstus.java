pietrobraione/jbse

commit 0456b8bbd9e3ef701a1f3ac409f466adf1f4bd29

Improve inequalities handling in Sicstus

src/jbse/dec/DecisionProcedureExternalInterfaceSicstus.java
@@ -616,6 +616,19 @@ class DecisionProcedureExternalInterfaceSicstus extends DecisionProcedureExterna
 						this.bdd.ref(this.predicate);
 						this.bdd.deref(operand);
 						break;
+                    case NE:
+                        final Primitive firstOp = e.getFirstOperand();
+                        final Primitive secondOp = e.getSecondOperand();
+                        if (Type.isPrimitiveIntegral(firstOp.getType()) && 
+                            Type.isPrimitiveIntegral(secondOp.getType())) {
+                            //sicstus clp(q) handles < and > better than =\=
+                            //so we translate A != B as (A < B) || (A > B);
+                            //since it is unclear the advantage in the 
+                            //noninteger case, we only do it for integers
+                            final Primitive val = firstOp.lt(secondOp).or(firstOp.gt(secondOp));
+                            val.accept(this);
+                            break;
+                        } //else fall through
 					default:
 						storeAtomicPredicate(e);
 						break;

commit 4fd326a2c1ae3475662e92da9628590d85fb944e

Fix performance regression introduced in commit 0456b8b

src/jbse/dec/DecisionProcedureExternalInterfaceSicstus.java
@@ -640,12 +640,17 @@ class DecisionProcedureExternalInterfaceSicstus extends DecisionProcedureExterna
                     case NE:
                         final Primitive firstOp = e.getFirstOperand();
                         final Primitive secondOp = e.getSecondOperand();
+                        //sicstus clp(q) handles < and > better than =\=
+                        //so we translate A != B as (A < B) || (A > B);
+                        //since it is unclear the advantage in the 
+                        //noninteger case, we only do it for integers,
+                        //except for the easy case var != const.
+                        //Note that this trick might stress a lot the 
+                        //boolean part as it makes 
                         if (Type.isPrimitiveIntegral(firstOp.getType()) && 
-                            Type.isPrimitiveIntegral(secondOp.getType())) {
-                            //sicstus clp(q) handles < and > better than =\=
-                            //so we translate A != B as (A < B) || (A > B);
-                            //since it is unclear the advantage in the 
-                            //noninteger case, we only do it for integers
+                            Type.isPrimitiveIntegral(secondOp.getType()) && 
+                            !( (firstOp instanceof PrimitiveSymbolic && secondOp instanceof Simplex) ||
+                               (firstOp instanceof Simplex && secondOp instanceof PrimitiveSymbolic))) {
                             final Primitive val = firstOp.lt(secondOp).or(firstOp.gt(secondOp));
                             val.accept(this);
                             break;