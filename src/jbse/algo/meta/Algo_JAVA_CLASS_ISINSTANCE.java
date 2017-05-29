458445aaab928abb603bfe387d69881d943b483a
Pietro Braione <pietro.braione@gmail.com>
2017-05-29 15:07:22 +0200

    Different error

--- src/jbse/algo/meta/Algo_JAVA_CLASS_ISINSTANCE.java
+++ src/jbse/algo/meta/Algo_JAVA_CLASS_ISINSTANCE.java
@@ -1,6 +1,7 @@
 package jbse.algo.meta;
 
 import static jbse.algo.Util.exitFromAlgorithm;
+import static jbse.algo.Util.failExecution;
 import static jbse.algo.Util.throwVerifyError;
 import static jbse.common.Type.isPrimitiveBinaryClassName;
 
@@ -17,46 +18,54 @@ import jbse.val.Reference;
 import jbse.val.Simplex;
 
 public final class Algo_JAVA_CLASS_ISINSTANCE extends Algo_INVOKEMETA_NONBRANCHING {
+    private String representedClass; //set by cookMore
+    private Reference tmpValue; //set by cookMore
+    
     @Override
     protected Supplier<Integer> numOperands() {
         return () -> 2;
     }
     
     @Override
-    protected void update(State state) 
+    protected void cookMore(State state)
     throws ThreadStackEmptyException, InterruptException {
-        //gets the 'this' java.lang.Class instance from the heap 
-        //and the name of the class it represents
-        Reference javaClassRef = null; //to keep the compiler happy;
         try {
-            javaClassRef = (Reference) this.data.operand(0);
+            //gets the 'this' java.lang.Class instance from the heap 
+            //and the name of the class it represents
+            final Reference javaClassRef = (Reference) this.data.operand(0);
+            if (state.isNull(javaClassRef)) {
+                //this should never happen
+                failExecution("The 'this' parameter to java.lang.Class.isInstance method is null.");
+            }
+            //TODO the next cast fails if javaClassRef is symbolic and expanded to a regular Instance. Handle the case.
+            final Instance_JAVA_CLASS javaClassObject = (Instance_JAVA_CLASS) state.getObject(javaClassRef);
+            if (javaClassObject == null) {
+                //this should never happen
+                failExecution("The 'this' parameter to java.lang.Class.isInstance method is symbolic and unresolved.");
+            }
+            this.representedClass = javaClassObject.representedClass();
+            
+            //gets the reference to the object to be checked
+            this.tmpValue = (Reference) this.data.operand(1);
         } catch (ClassCastException e) {
             throwVerifyError(state);
             exitFromAlgorithm();
         }
-        final Instance_JAVA_CLASS javaClassObject = (Instance_JAVA_CLASS) state.getObject(javaClassRef);
-        final String representedClass = javaClassObject.representedClass();
-
-        //gets the reference to the object to be checked
-        Reference tmpValue = null; //to keep the compiler happy
-        try {
-            tmpValue = (Reference) this.data.operand(1);
-        } catch (ClassCastException e) {
-            throwVerifyError(state);
-            exitFromAlgorithm();
-        }
-
+    }
+    
+    @Override
+    protected void update(State state) throws ThreadStackEmptyException {
         //determines which value to push on the operand stack
         final Simplex valToPush;
-        if (isPrimitiveBinaryClassName(representedClass) || state.isNull(tmpValue)) {
+        if (isPrimitiveBinaryClassName(this.representedClass) || state.isNull(this.tmpValue)) {
             valToPush = state.getCalculator().valInt(0);
         } else {
             //checks whether the object's class is a subclass 
             //of the class name from the constant pool
             final ClassHierarchy hier = state.getClassHierarchy();
-            final Objekt obj = state.getObject(tmpValue);
+            final Objekt obj = state.getObject(this.tmpValue);
             final String objClass = obj.getType();
-            valToPush = state.getCalculator().valInt(hier.isSubclass(objClass, representedClass) ? 1 : 0);
+            valToPush = state.getCalculator().valInt(hier.isSubclass(objClass, this.representedClass) ? 1 : 0);
         }
 
         //pushes the value
