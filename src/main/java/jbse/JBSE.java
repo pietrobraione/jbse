b7f9706cd32e5a1c4f4a62c7ff41e90ceb52de56
Pietro Braione <pietro.braione@gmail.com>
2017-09-19 16:17:01 +0200

    Fix version number

--- src/main/java/jbse/JBSE.java
+++ src/main/java/jbse/JBSE.java
@@ -13,7 +13,7 @@ public final class JBSE {
     public static final String ACRONYM = "JBSE";
 
     /** The current version of this project. */
-    public static final String VERSION = "0.7";
+    public static final String VERSION = "0.8.0-SNAPSHOT";
 
     /** The license under which this project is distributed. */
     public static final String LICENSE = "GNU GPL v.3.0 or greater";
