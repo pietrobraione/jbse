850ce8abde9d7b1483da9c0d1b480fea5e8096e7
Pietro Braione <pietro.braione@gmail.com>
2018-02-09 17:39:13 +0100

    Small fix

--- src/main/java/jbse/algo/meta/Algo_JBSE_BASE_CLINIT.java
+++ src/main/java/jbse/algo/meta/Algo_JBSE_BASE_CLINIT.java
@@ -151,9 +151,9 @@ public final class Algo_JBSE_BASE_CLINIT extends Algo_INVOKEMETA_Nonbranching {
         
         //initializes the paths
         JAVA_HOME             = cp.javaHome();
-        SUN_BOOT_CLASS_PATH   = stream(cp.bootClassPath()).reduce("", (s, t) -> s + PATH_SEPARATOR + t);
-        JAVA_EXT_DIRS         = stream(cp.extDirs()).reduce("", (s, t) -> s + PATH_SEPARATOR + t);
-        JAVA_CLASS_PATH       = stream(cp.classPath()).reduce("", (s, t) -> s + PATH_SEPARATOR + t);
+        SUN_BOOT_CLASS_PATH   = String.join(PATH_SEPARATOR, stream(cp.bootClassPath()).map(Object::toString).toArray(String[]::new));
+        JAVA_EXT_DIRS         = String.join(PATH_SEPARATOR, stream(cp.extDirs()).map(Object::toString).toArray(String[]::new));
+        JAVA_CLASS_PATH       = String.join(PATH_SEPARATOR, stream(cp.classPath()).map(Object::toString).toArray(String[]::new));
         SUN_BOOT_LIBRARY_PATH = JAVA_HOME + FILE_SEPARATOR + "lib";
         JAVA_LIBRARY_PATH     = JAVA_EXT_DIRS; //TODO this is an approximation, the real case is much more complex and os-dependent
         
