d11ef9ee6a3bc43e289ea3dba64f1c6fe1708f1d
Pietro Braione <pietro.braione@gmail.com>
2018-04-20 16:15:05 +0100

    Fix bug

--- src/main/java/jbse/apps/run/DecisionProcedureGuidanceJDI.java
+++ src/main/java/jbse/apps/run/DecisionProcedureGuidanceJDI.java
@@ -324,7 +324,7 @@ public final class DecisionProcedureGuidanceJDI extends DecisionProcedureGuidanc
                 } else if (origin instanceof KlassPseudoReference) {
                     return getJDIObjectStatic(((KlassPseudoReference) origin).getClassName());
                 } else if (origin instanceof SymbolicMemberField) {
-                    final Object o = getJDIValue(((PrimitiveSymbolicMemberArrayLength) origin).getContainer());
+                    final Object o = getJDIValue(((SymbolicMemberField) origin).getContainer());
                     if (!(o instanceof com.sun.jdi.ReferenceType) && !(o instanceof com.sun.jdi.ObjectReference)) {
                         throw new GuidanceException(ERROR_BAD_PATH);
                     }
