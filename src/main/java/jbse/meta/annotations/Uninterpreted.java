89cc536110acfd417a4a3fbcfda24251c891eb32
Pietro Braione <pietro.braione@gmail.com>
2019-02-20 22:47:02 +0100

    Small changes

--- src/main/java/jbse/meta/annotations/Uninterpreted.java
+++ src/main/java/jbse/meta/annotations/Uninterpreted.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
  * If a method is annotated with this annotation, when it is 
  * invoked at the base level the symbolic executor will not execute 
  * it, and instead will return a symbolic term. The term is the application 
- * of a function symbol, whose name is the method's signature,
+ * of a function symbol, whose name is specified in this annotation's value,
  * to the actual parameters of the method invocation, in the same order.
  * 
  * @author Pietro Braione
