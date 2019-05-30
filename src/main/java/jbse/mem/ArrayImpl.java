518b84314721f1c6a970b89eac59c611b998ef3e
Pietro Braione <pietro.braione@gmail.com>
2019-05-30 10:24:36 -0400

    Small change

--- src/main/java/jbse/mem/ArrayImpl.java
+++ src/main/java/jbse/mem/ArrayImpl.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-import java.util.LinkedList;
 import java.util.NoSuchElementException;
 import java.util.function.Consumer;
 
