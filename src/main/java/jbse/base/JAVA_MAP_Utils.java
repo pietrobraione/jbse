fd6feef8fee7fffb78afb01fa1778910883e178b
Pietro Braione <pietro.braione@gmail.com>
2020-12-01 11:15:18 +0100

    Small changes

--- src/main/java/jbse/base/JAVA_MAP_Utils.java
+++ src/main/java/jbse/base/JAVA_MAP_Utils.java
@@ -13,7 +13,6 @@ import jbse.val.Operator;
 import jbse.val.Primitive;
 import jbse.val.PrimitiveSymbolicMemberField;
 import jbse.val.ReferenceSymbolic;
-import jbse.val.Symbolic;
 import jbse.val.SymbolicApply;
 import jbse.val.SymbolicMemberField;
 import jbse.val.Value;
@@ -25,9 +24,9 @@ public class JAVA_MAP_Utils {
 		if (!(value instanceof SymbolicMemberField)) {
 			return false;
 		}
-		SymbolicMemberField originMemberField = (SymbolicMemberField) value;
+		final SymbolicMemberField originMemberField = (SymbolicMemberField) value;
 		if (binaryClassName(originMemberField.getFieldClass()).equals("java.util.HashMap") 
-				&& originMemberField.getFieldName().equals(INITIAL_MAP_FIELD_NAME)) {
+		&& originMemberField.getFieldName().equals(INITIAL_MAP_FIELD_NAME)) {
 			return true;
 		} else {
 			return false;
@@ -39,8 +38,8 @@ public class JAVA_MAP_Utils {
 		if (!(value instanceof SymbolicApply)) {
 			return false;
 		}
-		SymbolicApply symbolicApply = (SymbolicApply) value;
-		jbse.val.Value[] args = symbolicApply.getArgs();
+		final SymbolicApply symbolicApply = (SymbolicApply) value;
+		final Value[] args = symbolicApply.getArgs();
 		if (args.length > 0 && JAVA_MAP_Utils.isInitialMapField(args[0])) {
 			if (!symbolicApply.getOperator().equals("java/util/Map:(Ljava/lang/Object;)Z:containsKey")) {
 				throw new UnexpectedInternalException("Path condition refers to unexpected symbolicApply on a symbolic map: " + symbolicApply.getOperator());
@@ -53,15 +52,15 @@ public class JAVA_MAP_Utils {
 	
 	public static boolean assumptionViolated(Clause clause) {
 		if (clause instanceof ClauseAssumeReferenceSymbolic) {
-			ReferenceSymbolic ref = ((ClauseAssumeReferenceSymbolic) clause).getReference(); 
+			final ReferenceSymbolic ref = ((ClauseAssumeReferenceSymbolic) clause).getReference(); 
 			if (isInitialMapField(ref) && (clause instanceof ClauseAssumeAliases || clause instanceof ClauseAssumeNull)) {
 				return true;
 			} 
 		} else if (clause instanceof ClauseAssume) {
-			Expression cond = (Expression) ((ClauseAssume) clause).getCondition(); 
-			Primitive firstOp = cond.getFirstOperand();
+			final Expression cond = (Expression) ((ClauseAssume) clause).getCondition(); 
+			final Primitive firstOp = cond.getFirstOperand();
 			if (cond.getOperator().equals(Operator.LT) && firstOp instanceof PrimitiveSymbolicMemberField) {
-				PrimitiveSymbolicMemberField field = (PrimitiveSymbolicMemberField) firstOp;
+				final PrimitiveSymbolicMemberField field = (PrimitiveSymbolicMemberField) firstOp;
 				if (isInitialMapField(field.getContainer()) && "size".equals(field.getFieldName())) {
 					return true;
 				}
@@ -75,7 +74,7 @@ public class JAVA_MAP_Utils {
 			origin = origin.replace(".java/util/HashMap:" + INITIAL_MAP_FIELD_NAME, "");
 		}
 		if (origin.contains("::GET(")) {
-			String mapRef = origin.substring(0, origin.indexOf("::GET("));
+			final String mapRef = origin.substring(0, origin.indexOf("::GET("));
 			String keyRef = origin.substring(origin.indexOf("::GET(") + 6);
 			keyRef = keyRef.substring(0, keyRef.indexOf(')'));
 			origin = "<java/util/Map:(Ljava/lang/Object;)Ljava/lang/Object;:get@" + mapRef + "," + keyRef + ">";
