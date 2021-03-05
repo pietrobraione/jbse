8f77d627e97261b5ad3c5df86643c1657559c307
Pietro Braione <pietro.braione@gmail.com>
2021-03-05 12:45:14 +0100

    Refactor

--- src/main/java/jbse/algo/meta/Algo_JAVA_SYSTEM_SETOUT0.java
+++ src/main/java/jbse/algo/meta/Algo_JAVA_SYSTEM_SETOUT0.java
@@ -1,60 +1,14 @@
 package jbse.algo.meta;
 
-import static jbse.algo.Util.failExecution;
-import static jbse.bc.Signatures.JAVA_SYSTEM;
 import static jbse.bc.Signatures.JAVA_SYSTEM_OUT;
-
-import java.util.function.Supplier;
-
-import jbse.algo.Algo_INVOKEMETA_Nonbranching;
-import jbse.algo.StrategyUpdate;
-import jbse.algo.meta.exc.UndefinedResultException;
-import jbse.bc.ClassFile;
-import jbse.bc.exc.BadClassFileVersionException;
-import jbse.bc.exc.ClassFileIllFormedException;
-import jbse.bc.exc.ClassFileNotAccessibleException;
-import jbse.bc.exc.ClassFileNotFoundException;
-import jbse.bc.exc.IncompatibleClassFileException;
-import jbse.bc.exc.RenameUnsupportedException;
-import jbse.bc.exc.WrongClassNameException;
-import jbse.common.exc.InvalidInputException;
-import jbse.mem.Klass;
-import jbse.mem.State;
-import jbse.tree.DecisionAlternative_NONE;
 
 /**
  * Meta-level implementation of {@link java.lang.System#setOut0(java.io.PrintStream)}.
  * 
  * @author Pietro Braione
  */
-public final class Algo_JAVA_SYSTEM_SETOUT0 extends Algo_INVOKEMETA_Nonbranching {
-    private Klass k; //set by cookMore
-    
-    @Override
-    protected Supplier<Integer> numOperands() {
-        return () -> 1;
-    }
-    
-    
-    @Override
-    protected void cookMore(State state) throws UndefinedResultException, InvalidInputException, RenameUnsupportedException {
-        try {
-            final ClassFile cf_JAVA_SYSTEM = state.getClassHierarchy().loadCreateClass(JAVA_SYSTEM);
-            this.k = state.getKlass(cf_JAVA_SYSTEM);
-            if (this.k == null || !this.k.initializationCompleted()) {
-                throw new UndefinedResultException("Invoked java.lang.System.setIn0 before initialization of class java.lang.System.");
-            }
-        } catch (ClassFileNotFoundException | ClassFileIllFormedException | BadClassFileVersionException |
-                 WrongClassNameException | IncompatibleClassFileException | ClassFileNotAccessibleException e) {
-            //this should never happen
-            failExecution(e);
-        }
-    }
-
-    @Override
-    protected StrategyUpdate<DecisionAlternative_NONE> updater() {
-        return (state, alt) -> {
-            this.k.setFieldValue(JAVA_SYSTEM_OUT, this.data.operand(0));
-        };
-    }
+public final class Algo_JAVA_SYSTEM_SETOUT0 extends Algo_JAVA_SYSTEM_SETX0 {
+	public Algo_JAVA_SYSTEM_SETOUT0() {
+		super("setOut0", JAVA_SYSTEM_OUT);
+	}
 }
