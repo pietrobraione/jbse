bacf8c491564a1fe38e5a2266b06221c85cdced0
Pietro Braione <pietro.braione@gmail.com>
2021-06-29 15:02:48 +0200

    Small change

--- src/test/java/jbse/dec/DecisionProcedureTest.java
+++ src/test/java/jbse/dec/DecisionProcedureTest.java
@@ -44,7 +44,7 @@ public class DecisionProcedureTest {
 
     //BEGIN TO PATCH
     private static final ArrayList<String> COMMAND_LINE = Z3_COMMAND_LINE;
-    private static final String SMT_SOLVER_PATH = "/opt/local/bin/z3";//"/Users/denaro/Desktop/RTools/Z3/z3-4.3.2.d548c51a984e-x64-osx-10.8.5/bin/z3";
+    private static final String SMT_SOLVER_PATH = "/usr/bin/z3"; //"/opt/local/bin/z3";//"/Users/denaro/Desktop/RTools/Z3/z3-4.3.2.d548c51a984e-x64-osx-10.8.5/bin/z3";
     //END TO PATCH
 
     static {
